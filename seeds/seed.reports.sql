c2550a8eaf71847b345ea8402cf1c5c7204b6380
meta-434 <ach1@vt.edu>
2020-05-29 06:14:06 -0400

    add db seeds

---
+++ seeds/seed.reports.sql
@@ -0,0 +1,30 @@
+insert into reports (
+    report_first ,
+    report_last ,
+    report_email ,
+    report_phone ,
+    report_lat ,
+    report_lng ,
+    report_date ,
+    report_type ,
+    report_time,
+    report_waterBody ,
+    report_other ,
+    report_details
+)
+values
+    (
+        'Alexandre',
+        'Hapgood',
+        'ach1@vt.edu',
+        '434-249-7488',
+        38.09802480089654,
+        -78.4887256216315,
+        '2020-01-01',
+        '{"Suspicious discharge from pipe into stream", "Suspicious suds or other substance floating on water"}',
+        '12:34',
+        'James River',
+        'I feel pretty ok, how about you?',
+        'the quick brown fox jumped over the lazy dog'
+    )
+;
---
+++ seeds/seed.users.sql
@@ -0,0 +1,5 @@
+insert into users (username, password)
+values
+    ('acps-employee', 'ilovemywatershed'),
+    ('acps-admin', 'aSZ3yivxZ8uwg6UcthY')
+;
