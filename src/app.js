76637e422a2530a286fadd954e4cb61b1ef96d2c
meta-434 <ach1@vt.edu>
2020-05-29 06:13:20 -0400

    update app.js

--- src/app.js
+++ src/app.js
@@ -1,33 +1,129 @@
 require('dotenv').config();
 const express = require('express');
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+const config = require('./config');
+const xss = require('xss');
 const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
+const reportsRouter = require('./reports/reports-router');
+const AuthService = require('./auth-service');
+
 const app = express();
 
-const morganOption = (NODE_ENV === 'production')
-    ? 'tiny'
-    : 'common';
+//set secret
+app.set('Secret', config.SECRET_KEY);
 
-app.use(morgan(morganOption));
+// helmet
 app.use(helmet());
+
+// cors
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res
-        .send('Hello, world!')
+// use morgan to log requests to the console
+app.use(morgan('dev'));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+// define router
+const  ProtectedRoutes = express.Router();
+
+app.use('/api', ProtectedRoutes);
+ProtectedRoutes.use((req, res, next) =>{
+    // check header or url parameters or post parameters for token
+    let token = req.headers['access-token'];
+
+    // decode token
+    if (token) {
+        // verifies secret and checks exp
+        jwt.verify(token, app.get('Secret'), (err, decoded) =>{
+            if (err) {
+                return res.json({ success: false, message: 'Failed to authenticate token.' });
+            } else {
+                // if everything is good, save to request for use in other routes
+                req.decoded = decoded;
+                next();
+            }
+        });
+    } else {
+        // if there is no token, return an error
+        return res.status(403).send({
+            message: 'No token provided.'
+        });
+    }
 });
 
+// on successful authentication, return jwt to client for future access
+app.post('/authenticate',(req,res,next)=>{
+    const knexInstance = req.app.get('db');
+    AuthService.getAllUsers(knexInstance)
+        .then(users => {
+            return users;
+        })
+        .then(result => {
+            const foundUsers = result.filter((el) => {
+                return (
+                    el.username === req.body.username &&
+                    el.password === req.body.password
+                );
+            });
+            // if there are any users that match...
+            if (foundUsers.length > 0) {
+                //if everything is ok, proceed to create token
+                const payload = {
+                    check:  true,
+                    username: foundUsers[0].username,
+                    password: foundUsers[0].password,
+                    id: foundUsers[0].id,
+                    is_admin: foundUsers[0].admin,
+                };
+
+                let token = jwt.sign(payload, app.get('Secret'), {
+                    expiresIn: 1440 // expires in 24 hours
+                });
+
+                // return the information to the client
+                res
+                    .status(202)
+                    .json({
+                        message: 'authentication complete.',
+                        token: token
+                    })
+
+            } else {
+                res
+                    .status(403)
+                    .send({error: "username or password incorrect"})
+
+            }
+        })
+        .catch(next);
+})
+
+// point to other endpoint routers
+ProtectedRoutes.use('/reports', reportsRouter);
+
+// '/' catcher
+app.get('/', function(req, res) {
+    res.send(`Hello world! app is running on ${config.PORT}`);
+});
+
+// error handling
 app.use(function errorHandler(error, req, res, next) {
     let response;
     if (NODE_ENV === 'production') {
-        response = { error: { message: 'server error'}}
+        response = { error: 'Server error' }
     } else {
         console.error(error);
-        response = { error: { message: error.message, error}}
+        response = { message: error.message, error }
     }
-    res.status(500).json(response);
+    res.status(500).json(response).catch(next);
 });
 
 module.exports = app;
